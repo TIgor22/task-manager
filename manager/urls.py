613598c80e797d6168780ab4df27e9591e2a8485
Ihor Tutak <igor2tak2212@gmail.com>
2024-06-20 15:11:33 +0300

    "created an index function for the site's main page in views.py and the path to it"

---
+++ manager/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from manager.views import index
+
+app_name = "manager"
+
+urlpatterns = [
+    path("", index, name="index"),
+]
+
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Task, Worker
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    actual_tasks = Task.objects.filter(is_completed=False).count()
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+    workers = Worker.objects.count()
+
+    context = {
+        "actual_tasks": actual_tasks,
+        "completed_tasks": completed_tasks,
+        "workers": workers
+    }
+
+    return render(request, "manager/index.html", context=context)
