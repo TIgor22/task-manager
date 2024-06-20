f24367893c07a77637c413d5b0d34a92df462596
Ihor Tutak <igor2tak2212@gmail.com>
2024-06-20 17:58:12 +0300

    "add debug-toolbar in settings.py and path in urls.py"

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("manager.urls", namespace="manager")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
