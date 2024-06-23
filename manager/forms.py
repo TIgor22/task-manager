e44da1d153b8236a49debd16213f0dc6d4fc8be0
Ihor Tutak <igor2tak2212@gmail.com>
2024-06-23 18:31:16 +0300

    "created classes Task SearchForm and TaskForm in forms.py"

--- manager/forms.py
+++ manager/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from manager.models import TaskType, Position, Worker
+from manager.models import TaskType, Position, Worker, Task
 
 
 class TaskTypeForm(forms.ModelForm):
@@ -24,3 +25,31 @@ class WorkerForm(UserCreationForm):
         model = Worker
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "position",)
 
+
+class TaskSearchForm(forms.Form):
+    task = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by Task"}
+        )
+    )
+
+
+class TaskForm(forms.ModelForm):
+
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local"}
+        )
+    )
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
