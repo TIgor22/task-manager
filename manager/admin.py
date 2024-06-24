5c4ca2d3a75c5eb61c16572dcb56042133a7605a
Ihor Tutak <igor2tak2212@gmail.com>
2024-06-24 10:55:54 +0300

    "added fields first_name, last_name, position to add_fieldsets class Worker"

--- manager/admin.py
+++ manager/admin.py
@@ -22,6 +22,9 @@ class PositionAdmin(admin.ModelAdmin):
 class WorkerAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("position", )
     fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("position", )}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "position")}),
+    )
 
 
 @admin.register(Task)
